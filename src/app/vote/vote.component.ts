887aa6f72274ddb8fcdc203fdedc8cb20784237a
Bermos <matthew.machivenyika@hispeed.ch>
2018-03-14 18:11:08 +0100

    Lobby joining added
    The vote view now actually joins a lobby when opend and displays a success/error message when that task is done.

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -8,15 +8,20 @@ import { SocketService } from "../socket.service";
   styleUrls: ['./vote.component.css']
 })
 export class VoteComponent implements OnInit {
+  successMessage: string;
 
   constructor(private socket: SocketService, private route: ActivatedRoute) {
-    this.socket.send('vote state', {id: this.route.snapshot.params['id']});
-    this.socket.send('', {});
+    // Request to join the lobby with the id from the url
+    this.socket.send('join lobby', {id: this.route.snapshot.params['id']});
 
-
-    this.socket.subscribeTo('vote exists', msg => {
-      console.log('2nd:', msg);
-    });
+    // Listen if join request was accepted or denied
+    this.socket.subscribeTo('join lobby', msg => {
+      if (msg) {
+        this.successMessage = 'Joined lobby!';
+      } else {
+        alert('No lobby with this id exists!');
+      }
+    })
   }
 
   ngOnInit() {
