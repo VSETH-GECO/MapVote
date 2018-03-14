9385fdc1499803c43e0ef23d5ca64e6e8806a9d4
Bermos <matthew.machivenyika@hispeed.ch>
2018-03-14 18:08:24 +0100

    Cleanup and comments
    Added comments to the web socket handler on the backend as well as removed unused lines.

--- server/ws-handler.js
+++ server/ws-handler.js
@@ -3,18 +3,21 @@ const vm = require('./votemanager');
 class WSHandler {
   static initialise(socket) {
 
+    // Join lobby event. Checks if the requested lobby exists
+    // and if so joins that socket to a room with the same id
+    // as the vote
     socket.on('join lobby', msg => {
       if (vm.getLobbyByID(msg.id)) {
         socket.emit('join lobby', true);
         socket.join(msg.id);
 
-        //vm.addSocketToLobby(socket, msg.id);
       } else {
         socket.emit('join lobby', false)
       }
     });
 
-    socket.on('vote state', msg => {
+    // Responds to the question if a vote with a specific id exists
+    socket.on('vote exists', msg => {
       socket.emit('vote exists', (vm.getLobbyByID(msg.id) !== null));
     });
   }
