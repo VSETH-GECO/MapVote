a7edd808503b88f2747a38766c731947cd743efc
Bermos <matthew.machivenyika@hispeed.ch>
2018-03-14 14:39:54 +0100

    Implemented basic functionality for creator view
    The creator view that is used to create a new lobby lets you select a game and a voting method for it.
    Still left to do is the actual request that will create the lobby on the server.

--- src/app/creator/creator.component.ts
+++ src/app/creator/creator.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+class Game {
+  maps: string[];
+}
+class Mode {
+  name: string;
+}
 
 @Component({
   selector: 'app-creator',
@@ -6,10 +14,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creator.component.css']
 })
 export class CreatorComponent implements OnInit {
+  /* games: Game[] = [
+    {
+      name: 'Overwatch',
+      maps: [
+        {name: 'Château Guillard'},
+        {name: 'Dorado'},
+        {name: 'Eichenwalde'},
+        {name: 'Hanamura'},
+        {name: 'Hollywood'},
+        {name: 'Horizon Lunar Colony'},
+        {name: 'Ilios'},
+        {name: 'King\'s Row'},
+        {name: 'Lijiang Tower'},
+        {name: 'Nepal'},
+        {name: 'Numbani'},
+        {name: 'Oasis'},
+        {name: 'Route 66'},
+        {name: 'Temple of Anubis'},
+        {name: 'Volskaya Industries'},
+        {name: 'Watchpoint: Gibraltar'}
+        ]
+    },
+    {
+      name: 'Counter Strike: GO',
+      maps: [
+        {name: 'Flashpoint'},
+        {name: 'Test 1'}
+        ]
+    }
+  ]; */
+  modes: Mode[] = [
+    {name: "ping-pong"},
+  ];
+  games: string[];
+  stage: number;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.stage = 0;
+    this.http.get<string[]>(window.location.origin + '/api/games').subscribe(ans => {
+      this.games = ans;
+      console.log(this.games);
+    });
   }
 
+  selectGame(game) {
+    console.log('Game:', game);
+    this.stage = 1;
+  }
+
+  selectMode(mode) {
+    console.log('Mode', mode);
+    this.stage = 2;
+  }
+
+  createLobby() {
+    // TODO
+  }
 }
